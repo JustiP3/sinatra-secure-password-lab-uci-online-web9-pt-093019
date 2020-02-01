39c93a09d55327c3ed40b0f4246e7e17da36ef57
Justin Phillips <justinphil02@yahoo.com>
2020-02-01 06:43:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,10 +25,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
-      @user = User.find(session[:user_id])
-      erb :account
-
+    @user = User.find(session[:user_id])
+    erb :account
   end
 
 
